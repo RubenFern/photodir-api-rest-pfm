1cb10a66c39567cd1222171737ab4f0b6acf4bc5
RubenFern <rubenferval@hotmail.com>
2021-04-19 20:52:03 +0200

    Controlador para las funciones del administrador creado. Función para mostrar todos los usuarios creada

---
+++ controllers/adminController.js
@@ -0,0 +1,18 @@
+const {request, response} = require('express');
+
+const User = require('../models/userSchema');
+
+
+const viewUsers = async(req = request, res = response) =>
+{
+    const users = await User.find();
+
+    res.json({
+        users
+    });
+}
+
+
+module.exports = {  
+    viewUsers
+}
