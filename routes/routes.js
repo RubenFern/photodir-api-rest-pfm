1d38481a8bcc06804977c3641e4cb27a9dce2382
RubenFern <rubenferval@hotmail.com>
2021-04-19 20:50:28 +0200

    Ruta para el panel de administrador añadida

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const routes = (app) =>
     // Ruta principal de la apliación
     app.use('/', require('./userPath'));
     app.use('/', require('./authPath'));
+
+    // Rutas para los administradores
+    app.use('/panel', require('./adminPath'));
 }
 
 module.exports = routes;
