cfb244bf4daf0ded37cd94887c40acaa8c9f8b24
RubenFern <rubenferval@hotmail.com>
2021-04-20 20:21:40 +0200

    Esquema de la colección de los albumes

---
+++ models/albumSchema.js
@@ -0,0 +1,52 @@
+const {Schema, model} = require('mongoose');
+
+const AlbumSchema = Schema
+({
+    uid_user: 
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'El álbum debe pertenecer a un usuario']
+    },
+
+    name:
+    {
+        type: String,
+        required: [true, 'El nombre del álbum es obligatorio'],
+        unique: true
+    },
+
+    description: 
+    {
+        type: String,
+    },
+
+    image:
+    {
+        type: String,
+        default: 'album_image.jpg'
+    },
+
+    creation_date:
+    {
+        type: String,
+        default: () =>
+        {
+            const date = new Date();
+
+            return date.toLocaleString();
+        }
+    }
+});
+
+AlbumSchema.methods.toJSON = function()
+{
+    const {__v, _id, ...album} = this.toObject();
+
+    // Cambio el nombre de _id a uid
+    album.uid = _id;
+
+    return album;
+}
+
+module.exports = model('Album', AlbumSchema);
