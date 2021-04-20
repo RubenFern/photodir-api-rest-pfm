30c39c12506afdad59de5d37fbbde3e547494bc3
RubenFern <rubenferval@hotmail.com>
2021-04-20 20:18:58 +0200

    Cambio de nombre del archivo de los mensajes de error

--- routes/authPath.js
+++ routes/authPath.js
@@ -1,14 +1,15 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
+
 const { login } = require("../controllers/authController");
-const { validateUser } = require("../middlewares/validateUser");
+const showErros = require("../middlewares/showErrors");
 
 const router = Router();
 
-router.post('/login', [
+router.post('/', [
     check('user_name', 'Debes introducir tu nombre de usuario').notEmpty(),
     check('password', 'Debes introducir tu contraseña').notEmpty(),
-    validateUser
+    showErros
 ], login);
 
 module.exports = router;
