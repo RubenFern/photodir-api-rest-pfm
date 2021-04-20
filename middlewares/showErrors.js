458c24d8ba024efbda58f4d4044536a0741861a9
RubenFern <rubenferval@hotmail.com>
2021-04-20 20:21:22 +0200

    Cambio de nombre

---
+++ middlewares/showErrors.js
@@ -0,0 +1,15 @@
+const { validationResult } = require('express-validator');
+
+const showErros = (req, res, next) =>
+{
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+    {
+        return res.status(400).json(errors);
+    }
+
+    next();
+}
+
+module.exports = showErros;
