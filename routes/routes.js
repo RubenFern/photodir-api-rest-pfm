918f6f745c6ff78263fec468f2ea28c8fc148e71
RubenFern <rubenferval@hotmail.com>
2021-04-21 22:21:03 +0200

    Validación de Token en las ruta para eliminar y editar

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
 const routes = (app) =>
 {
     // Ruta principal de la apliación
-    app.use('/', require('./userPath'));
-    app.use('/login', require('./authPath'));
-    app.use('/albumes', require('./albumPath'));
+    app.use('/api', require('./userPath'));
+    app.use('/api/login', require('./authPath'));
+    app.use('/api/albumes', require('./albumPath'));
 
     // Rutas para los administradores
-    app.use('/panel', require('./adminPath'));
+    app.use('/api/panel', require('./adminPath'));
 }
 
 module.exports = routes;
