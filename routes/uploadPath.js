a0aae1582d97607b259754f01a9e378685577ae3
RubenFern <rubenferval@hotmail.com>
2021-05-05 22:17:49 +0200

    Cambio de nombre del controlador

--- routes/uploadPath.js
+++ routes/uploadPath.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 
-const { uploadImage } = require("../controllers/uploadController");
+const { loadImage } = require("../controllers/uploadController");
 
 const showErros = require("../middlewares/showErrors");
 const validateJWT = require("../middlewares/validateJWT");
 
 const router = Router();
 
-router.post('/', uploadImage);
+router.post('/', validateJWT, loadImage);
 
 module.exports = router;
