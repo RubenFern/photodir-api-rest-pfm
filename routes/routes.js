9c0c53577caf236332a7720c3c8ba55eb17c356b
RubenFern <rubenferval@hotmail.com>
2021-05-15 21:45:22 +0200

    Endpoints para la búsqueda de usuarios

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const routes = (app) =>
     app.use('/api/auth', require('./authPath'));
     app.use('/api/albumes', require('./albumPath'));
     app.use('/api/fotografias', require('./photoPath'));
+    app.use('/api/explore', require('./explorePath'));
     
     app.use('/api/upload', require('./uploadPath'));
     
