d297244fce8fe6dfa4c42041baa09519289fc0eb
RubenFern <rubenferval@hotmail.com>
2021-05-22 19:17:39 +0200

    Añadidias las rutas de likes y reportes

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,13 @@
 const routes = (app) =>
 {
     // Ruta principal de la apliación
-    app.use('/api', require('./userPath'));
+    app.use('/api/user', require('./userPath'));
     app.use('/api/auth', require('./authPath'));
     app.use('/api/albumes', require('./albumPath'));
     app.use('/api/fotografias', require('./photoPath'));
     app.use('/api/explore', require('./explorePath'));
     app.use('/api/likesphoto', require('./likePath'));
+    app.use('/api/report', require('./reportPath'));
     
     app.use('/api/upload', require('./uploadPath'));
     
