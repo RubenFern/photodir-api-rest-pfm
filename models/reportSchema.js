d3e9f1c63aa8bee54efec3f4a167fc32bd1b9904
RubenFern <rubenferval@hotmail.com>
2021-05-22 19:17:53 +0200

    Sistema de reportes de las imágenes completo

---
+++ models/reportSchema.js
@@ -0,0 +1,78 @@
+const {Schema, model} = require('mongoose');
+
+const ReportSchema = Schema
+({
+    // uid de la imagen reportada
+    uid_reported:
+    {
+        type: Schema.Types.ObjectId,
+        required: [true, 'Debes introducir el uid de la imagen reportada']
+    },
+
+    // nombre de la imagen reportada
+    image_reported:
+    {
+        type: String,
+        required: [true, 'Debes introducir la imagen a reportar']
+    },
+
+    // usuario que fue reportada
+    user_reported:
+    {
+        type: String,
+        required: [true, 'Debes especificar el usuario denunciado']
+    },
+
+    // usuario que reporta
+    reporting_user:
+    {
+        type: String,
+        required: [true, 'Debes especificar el usuario denunciante']
+    },
+
+    description:
+    {
+        type: String,
+        required: [true, 'Debes indicar el motivo del reporte']
+    },
+
+    category:
+    {
+        type: String,
+        enum: ['avatar', 'album', 'photo'],
+        required: [true, 'Debes indicar la categoría']
+    },
+
+    state:
+    {
+        type: String,
+        enum: ['approved', 'rejected', 'pending'], // aprobada, rechazada, pendiente
+        required: [true, 'Debes indicar el estado'],
+        default: 'pending'
+    },
+
+    creation_date:
+    {
+        type: String,
+        default: () =>
+        {
+            const date = new Date();
+
+            return date.toLocaleString();
+        }
+    }
+});
+
+// Impido que el JSON me devuelva la contraseña y la versión para tener solo los datos del usuario con los que trabajar
+ReportSchema.methods.toJSON = function()
+{
+    const {__v, password, _id, ...report} = this.toObject();
+
+    // Cambio el nombre de _id a uid
+    report.uid = _id;
+
+    return report;
+}
+
+// Exporto el nombre de la colección (Mongo añade una 's' al final) y el esquema de los atributos
+module.exports = model('Report', ReportSchema);
